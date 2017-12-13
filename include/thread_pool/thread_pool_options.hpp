4c03bfe46103b5861c4f5379aebd2884cf4b3701
Sever Topan <sever.topan@synaptivemedical.com>
2017-12-13 17:57:42 -0500

    Threadpool options function inlining.

--- include/thread_pool/thread_pool_options.hpp
+++ include/thread_pool/thread_pool_options.hpp
@@ -53,22 +53,22 @@ ThreadPoolOptions::ThreadPoolOptions()
 {
 }
 
-void ThreadPoolOptions::setThreadCount(size_t count)
+inline void ThreadPoolOptions::setThreadCount(size_t count)
 {
     m_thread_count = std::max<size_t>(1u, count);
 }
 
-void ThreadPoolOptions::setQueueSize(size_t size)
+inline void ThreadPoolOptions::setQueueSize(size_t size)
 {
     m_queue_size = std::max<size_t>(1u, size);
 }
 
-size_t ThreadPoolOptions::threadCount() const
+inline size_t ThreadPoolOptions::threadCount() const
 {
     return m_thread_count;
 }
 
-size_t ThreadPoolOptions::queueSize() const
+inline size_t ThreadPoolOptions::queueSize() const
 {
     return m_queue_size;
 }
