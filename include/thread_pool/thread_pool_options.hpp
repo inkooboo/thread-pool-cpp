af95dd88daa094f67bbd178b639c7282373a3b09
Andrey Kubarkov <inkooboo@gmail.com>
2018-02-06 18:41:48 +0000

    Fix multiple link units
    https://github.com/inkooboo/thread-pool-cpp/issues/16

--- include/thread_pool/thread_pool_options.hpp
+++ include/thread_pool/thread_pool_options.hpp
@@ -47,28 +47,28 @@ private:
 
 /// Implementation
 
-ThreadPoolOptions::ThreadPoolOptions()
+inline ThreadPoolOptions::ThreadPoolOptions()
     : m_thread_count(std::max<size_t>(1u, std::thread::hardware_concurrency()))
     , m_queue_size(1024u)
 {
 }
 
-void ThreadPoolOptions::setThreadCount(size_t count)
+inline void ThreadPoolOptions::setThreadCount(size_t count)
 {
     m_thread_count = std::max<size_t>(1u, count);
 }
 
-void ThreadPoolOptions::setQueueSize(size_t size)
+inline void ThreadPoolOptions::setQueueSize(size_t size)
 {
     m_queue_size = std::max<size_t>(1u, size);
 }
 
-size_t ThreadPoolOptions::threadCount() const
+inline size_t ThreadPoolOptions::threadCount() const
 {
     return m_thread_count;
 }
 
-size_t ThreadPoolOptions::queueSize() const
+inline size_t ThreadPoolOptions::queueSize() const
 {
     return m_queue_size;
 }
